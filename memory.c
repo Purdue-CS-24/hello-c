2c9008355008799a80b9f7a930a0cd58237b3604
Andrew Davis <drew@drewdavis.me>
2021-01-18 20:45:23 -0500

    Fix tabs

--- memory.c
+++ memory.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>							// String functions
+#include <string.h>				// String functions
 
 int main()
 {
-	int *p = malloc(sizeof(int));			// Create a new int
+	int *p = malloc(sizeof(int));		// Create a new int
 	*p = 25;
 	printf("%d\n", *p);
 
 	char *str = malloc(6 * sizeof(char));	// Create a new string
-	strcpy(str, "Hello");					// Cannot simply set a string, must use
-	printf("%s\n", str);					// a function to copy char by char
+	strcpy(str, "Hello");			// Cannot simply set a string, must use
+	printf("%s\n", str);			// a function to copy char by char
 
-	free(p);								// Clean up
-	free(str);								// after ourselves
+	free(p);				// Clean up
+	free(str);				// after ourselves
 }
